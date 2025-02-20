8896f6fccc342b30b0f2b585ca28be57629e4bee
Protostar5248 <pierremoutel100@gmail.com>
2025-02-20 10:18:15 -0800

    glitch fixed

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 
 var ctx = gameCanvas.getContext("2d");
 
-// var percentWidth = 100;
-// var percentHeight = 92;
+var percentWidth = 100;
+var percentHeight = 92;
 
-// function vh(percentHeight) {
-//     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-//     return (percentHeight * h) / 100;
-// }
+function vh(percentHeight) {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (percentHeight * h) / 100;
+}
 
-// function vw(percentWidth) {
-//     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-//     return (percentWidth * w) / 100;
-// }
+function vw(percentWidth) {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    return (percentWidth * w) / 100;
+}
 
-// var game_width = vw(percentWidth);
-// var game_height = (game_width / 8) * 6;
-// if (game_height > screen.height) {
-//     console.log("ggg")
-//     game_height=0
-//     game_width=0
-//     game_height = vh(percentHeight);
-//     game_width = (game_height * 8) / 6;
-// }
-// console.log("game_width", game_width)
+var game_width = vw(percentWidth);
+var game_height = (game_width / 8) * 6;
+if (game_height > screen.height) {
+    console.log("ggg")
+    game_height=0
+    game_width=0
+    game_height = vh(percentHeight);
+    game_width = (game_height * 8) / 6;
+}
+console.log("game_width", game_width)
 
 
-var game_height=gameCanvas.style.height;
-var game_width=gameCanvas.style.width;
+// var game_height=gameCanvas.style.height;
+// var game_width=gameCanvas.style.width;
 // flag arrays
 //const flags=['russia','albania','belgium','bosnia_and_herzegovina','bulgaria','czechia','estonia','france','germany','ireland','italy','lithuania','malta','san-marino','serbia','spain','sweden','switzerland','ukraine','vatican_city']
 //const flags=['afghanistan','albania','algeria','andorra','angola','atigua_and_barbuda','arab_league','argentina','armenia','aruba','australia','austria','azerbaijan','bahamas','bahrain','bangladesh','barbados','belarus','belgium','belize','benin','bhutan','bolivia','bonaire','bosnia_and_herzegovina','botswana','brazil','bruinei','bulgaria','burkina_faso','burundi','cambodia','cameroon','canada','cape_verde','central_african_republic','chad','chile','china','colombia','comoros','costa_rica',"cote_d'ivoire",'croatia','cuba','curacao','cuprus','czechia','democratic_republic_of_congo','denmark','djibouti','dominica','dominican_republic','east_timor','ecuador','egypt','el_salvador','england','equatorial_guinea','eritrea','estonia','ethiopia','europe','faroe_islands','fiji','finland','france','gabon','gambia','georgia','germany','ghana','greece','greenland','grenada','guadeloupe','guam','guatemala','guinea','bissau ']
@@ -46,7 +46,7 @@ var rectX = centreX - rectWidth / 2
 var changeX = 0;
 
 // target
-var targetCountryName = 'UK';
+var targetCountryName = '';
 var nTarget = 0;
 
 // score
@@ -60,6 +60,7 @@ var endOfGame = 'no';
 var isNextLevel = 'false';
 var nextLevel = 'no';
 var level = 0;
+var nextlevelbuttonhtml=document.getElementById('nextlevelbutton');
 // var newflag=level*5;
 // const flagsOnScreen=[shuffledFlagImgs[newflag],shuffledFlagImgs[newflag+1],shuffledFlagImgs[newflag+2],shuffledFlagImgs[newflag+3],shuffledFlagImgs[newflag+4]]
 
@@ -74,28 +75,28 @@ var YouWinGame = 'no'
 // homeButton.innerText = 'Home'
 // homeButton.id = 'homeButton'
 
-// homeButton.addEventListener('click', () => {
-
-// })
 
 
+
+function nextlevelclicked() {
+level += 5;
+    playGameLevel();
+    nextlevelbuttonhtml.hidden=true;
+}
 
 // next level button
-const nextLevelButton = document.createElement('button')
-nextLevelButton.innerText = 'Next Level'
-nextLevelButton.id = 'nextLevelButton'
+// const nextLevelButton = document.createElement('button')
+// nextLevelButton.innerText = 'Next Level'
+// nextLevelButton.id = 'nextLevelButton'
+// nextLevelButton.hidden = true;
 
-nextLevelButton.addEventListener('click', () => {
-    level += 5;
-    playGameLevel();
-    nextLevelButton.hidden = true;
-    Element.style.position='relative';
+// nextLevelButton.addEventListener('click', () => {
+//     level += 5;
+//     playGameLevel();
+//     nextLevelButton.hidden = true;
+//     // Element.style.position='relative';
     
-})
-nextLevelButton.hidden = true;
-nextLevelButton.style.display='flex';
-nextLevelButton.style.justifyContent='center';
-nextLevelButton.style.width=window.innerWidth/2;
+// })
 
 
 
@@ -108,17 +109,13 @@ nextLevelButton.style.width=window.innerWidth/2;
 // moveRightButton.addEventListener('click', () => {
 //     changeX=4;
 // })
-const div_canvas = document.getElementById('div-canvas-background');
-const canvas = document.getElementById('gameCanvas');
-const playAgainDiv =document.getElementById('play-again-div')
-const hello = document.getElementById('hellodiv');
-const header= document.getElementById('header');
-function insertAfternextlevelbutton() {
-header.insertBefore(nextLevelButton, hello);
-}
+// const canvas = document.getElementById('gameCanvas');
+// const playAgainDiv =document.getElementById('play-again-div')
+// const hello = document.getElementById('hellodiv');
+// const header= document.getElementById('header');
+// header.insertBefore(nextLevelButton, hello);
 // div_canvas.insert(moveRightButton, canvas);
 
-insertAfternextlevelbutton(nextLevelButton);
 
 
 
@@ -164,11 +161,18 @@ function playGameLevel() {
 
     // mainloop
     function mainLoop() {
+        
+
         clearScreenAndShowScore();
+        
         rectOffScreen();
+        
         drawTargetFlagTextAndRect();
+        
         changeTargetFlag();
+        
         updateCoordinatesAndDrawFlags();
+        
         // gameOver();
     }
 
@@ -318,9 +322,11 @@ function playGameLevel() {
         ctx.clearRect(0, 0, game_width, game_height);
         ctx.fillText("Next Level!", halfofgame_width - 90, 230);
         ctx.fillText("Score: " + score, halfofgame_width - 65, 280);
-        nextLevelButton.hidden = false;
-    }
+        nextlevelbuttonhtml.hidden = false;
+       y=[0,0,0,0,0]
+       x = shuffleArray(unshuffledX)
 }
+    }
 
 // utility functions
 
